efa0c1756641d9379fbab3a7201f1c2db8dc9b26
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-20 12:47:43 +0200

    uppstart grund angular-sida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Calculate } from './pages/calculate/calculate';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Home },
+    { path: "calculate", component: Calculate },
+    { path: "about", component: About }
+];
---
+++ src/app/pages/calculate/calculate.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculate',
+  imports: [],
+  templateUrl: './calculate.html',
+  styleUrl: './calculate.scss',
+})
+export class Calculate {}
