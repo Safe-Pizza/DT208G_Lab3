57145a6e443df0d366099d08527a4eca63c492f9
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-20 13:12:13 +0200

    skapar undersidor + komponeneter header, footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Mainfooter } from "./partials/mainfooter/mainfooter";
+import { Mainheader } from "./partials/mainheader/mainheader";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Mainfooter, Mainheader],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
