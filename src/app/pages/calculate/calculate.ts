f8736f08e016ebeeb3c7c3d36aea1b0d7a39e7ad
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-21 14:02:08 +0200

    styling för calculate-sidan

--- src/app/pages/calculate/calculate.ts
+++ src/app/pages/calculate/calculate.ts
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './calculate.scss',
 })
 export class Calculate {
-  //properties
+  //variabler
   numLenght: number = 0;
   numTemp: number = 0;
   resultLenght: string = "";
@@ -16,23 +16,35 @@ export class Calculate {
   unitTemp: string = "";
   unitLength: string = "";
 
+//funktioner
+  calcCelToFarenheit(): void { //konvertera celsius till farenheit
+    //kontroll om 0
+    if(this.numTemp === 0) return;
 
-  calcCelToFarenheit(): void {
     this.resultTemp = (this.numTemp * 1.8 + 32).toFixed(0);
     this.unitTemp = "farenheit";
   }
 
-  calcFarToCelsius(): void {
+  calcFarToCelsius(): void { //konvertera farenheit till celsius
+    //kontroll om 0
+    if(this.numTemp === 0) return;
+
     this.resultTemp = ((this.numTemp - 32) / 1.8).toFixed(0);
     this.unitTemp = "celsius";
   }
 
-  calcFotToMeter(): void {
+  calcFotToMeter(): void { //konvertera fot till meter
+    //kontroll om 0
+    if(this.numLenght === 0) return;
+
     this.resultLenght = (this.numLenght * 0.3048).toFixed(2);
     this.unitLength = "meter";
   }
 
-  calcMeterToFot(): void {
+  calcMeterToFot(): void { //kovertera meter till fot
+    //kontroll om 0
+    if(this.numLenght === 0) return;
+
     this.resultLenght = (this.numLenght * 3.28).toFixed(2);
     this.unitLength = "fot";
   }
