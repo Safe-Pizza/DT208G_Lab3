e72bfbcc55c2b6ef7d252c1632fe089398acf751
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-21 11:58:31 +0200

    Skriver funktioner för kovertinerg av temp och längd + html sida calculate

--- src/app/pages/calculate/calculate.ts
+++ src/app/pages/calculate/calculate.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculate',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculate.html',
   styleUrl: './calculate.scss',
 })
-export class Calculate {}
+export class Calculate {
+  //properties
+  numLenght: number = 0;
+  numTemp: number = 0;
+  resultLenght: string = "";
+  resultTemp: string = "";
+  unitTemp: string = "";
+  unitLength: string = "";
+
+
+  calcCelToFarenheit(): void {
+    this.resultTemp = (this.numTemp * 1.8 + 32).toFixed(0);
+    this.unitTemp = "farenheit";
+  }
+
+  calcFarToCelsius(): void {
+    this.resultTemp = ((this.numTemp - 32) / 1.8).toFixed(0);
+    this.unitTemp = "celsius";
+  }
+
+  calcFotToMeter(): void {
+    this.resultLenght = (this.numLenght * 0.3048).toFixed(2);
+    this.unitLength = "meter";
+  }
+
+  calcMeterToFot(): void {
+    this.resultLenght = (this.numLenght * 3.28).toFixed(2);
+    this.unitLength = "fot";
+  }
+}
