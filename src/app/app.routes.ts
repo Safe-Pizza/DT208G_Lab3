ba4d933fdb2e8e254229bf38ff32c287faea93e1
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-22 15:29:29 +0200

    lägger till 404-sida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Calculate } from './pages/calculate/calculate';
 import { About } from './pages/about/about';
+import { PageNotFound } from './pages/page-not-found/page-not-found';
 
 export const routes: Routes = [
     { path: "home", component: Home },
     { path: "calculate", component: Calculate },
     { path: "about", component: About },
-    { path: "", redirectTo: "home", pathMatch: "full"}
+    { path: "", redirectTo: "home", pathMatch: "full"},
+    { path: "**", component: PageNotFound }
 ];
