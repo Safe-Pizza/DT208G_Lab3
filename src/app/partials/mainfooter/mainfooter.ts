ccd5021d9ce7a9fd7ed8cc26817d9caca991eb18
Hanna Lindkvist <hali2507@student.miun.se>
2026-04-22 15:45:03 +0200

    skapar datum i footer

--- src/app/partials/mainfooter/mainfooter.ts
+++ src/app/partials/mainfooter/mainfooter.ts
@@ -1,9 +1,13 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-mainfooter',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './mainfooter.html',
   styleUrl: './mainfooter.scss',
 })
-export class Mainfooter {}
+export class Mainfooter {
+
+  today: Date = new Date();
+}
